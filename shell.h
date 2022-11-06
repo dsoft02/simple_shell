73ae5e8d6043fee14cc557bbc4fdef02ecc98a3f
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-11-06 01:38:26 -0700

    added the header file with all include file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#endif
